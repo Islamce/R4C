8377b3bdcf7a284a23c8a39618ed9701227d71ca
Islamce <sam612345@gmail.com>
2026-07-19 22:12:09 +0300

    Initialize R4C: add apps/api/src/main.ts

---
+++ apps/api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1");
+  app.enableShutdownHooks();
+  await app.listen(Number(process.env.API_PORT ?? 4000), "0.0.0.0");
+}
+
+void bootstrap();
